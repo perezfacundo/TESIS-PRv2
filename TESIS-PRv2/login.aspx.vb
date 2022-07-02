23bc4e62625979a80f658969228899bc75c915d0
fperez <PEREZFAQ98-2020@ies21.onmicrosoft.com>
2022-07-02 16:05:17 -0300

    Agregar archivos de proyecto.

---
+++ TESIS-PRv2/login.aspx.vb
@@ -0,0 +1,11 @@
+﻿Public Class login
+    Inherits System.Web.UI.Page
+
+    Private _businessLogicLayer As BusinessLogicLayer
+
+    Protected Sub Page_Load(ByVal sender As Object, ByVal e As System.EventArgs) Handles Me.Load
+        Dim _businessLogicLayer = New BusinessLogicLayer()
+    End Sub
+
+
+End Class
